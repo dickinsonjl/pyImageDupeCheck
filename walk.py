ea53260bd4f3c225656c5e40fe95bb315e1c4819
john_dickinson <dickinsonjl@gmail.com>
2016-11-05 15:39:42 +0000

    code tidy up, add hash-bang

--- walk.py
+++ walk.py
@@ -1,56 +1,58 @@
+#!/usr/bin/python -u
+# encoding: utf8
 import os
 import sqlite3
 
-sqliteFileName = 'test_sqlite.db';
+sqliteFileName = 'test_sqlite.db'
 
 if(os.path.isfile(sqliteFileName)):
-	createTables = False;
+    createTables = False
 else:
-	createTables = True;
-	print "Database not found, will be created:", sqliteFileName;
+    createTables = True
+    print "Database not found, will be created:", sqliteFileName
 
 conn = sqlite3.connect('test_sqlite.db')
 print "Opened database successfully"
 
 if(createTables):
-	conn.execute('''CREATE TABLE fileIndex
+    conn.execute('''CREATE TABLE fileIndex
         (ID INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
         path           TEXT    NOT NULL,
         filename           TEXT    NOT NULL,
-        size            INT     NOT NULL);''')
-	conn.commit();
-	print "Table created successfully";
+        size            INT     NOT NULL)''')
+    conn.commit()
+    print "Table created successfully"
 
 home = os.path.expanduser("~")
-duplicateSpaceUsed = 0;
-for dirpath, dirs, files in os.walk("."):	 
-	path = dirpath.split('/')
-	print '|', (len(path))*'---', '[',os.path.basename(dirpath),']'
-	for f in files:
-		this_fullpath = dirpath + '/' + f
-		this_filesize = os.path.getsize(this_fullpath)
-		#print '|', len(path)*'---', f, ' ', this_filesize
-		cursor = conn.execute("SELECT ID, path FROM fileIndex WHERE filename=? AND size=?", (f, str(this_filesize)))
-		result_length = len(cursor.fetchall())
-		if(result_length == 0):
-			conn.execute("INSERT INTO fileIndex (path, filename, size) VALUES(?, ?, ?)", (this_fullpath, f, str(this_filesize)))
-			print "inserted: ", this_fullpath
-		else:
-			cursor = conn.execute("SELECT ID, path FROM fileIndex WHERE filename=? AND size=?", (f, str(this_filesize)))
-			found_file_path = False
-			for row in cursor:
-				if(row[1] == this_fullpath):
-					found_file_path = True
-				else:
-					print "Found Duplicate:", this_fullpath, " in:", row[1]
-					duplicateSpaceUsed += this_filesize;
-					print "duplicate Total=", duplicateSpaceUsed;
-			if(found_file_path == False):
-				print "Unique file:", this_fullpath
-				conn.execute("INSERT INTO fileIndex (path, filename, size) VALUES(?, ?, ?)", (this_fullpath, f, str(this_filesize)))
-				print "inserted: ", this_fullpath
+duplicateSpaceUsed = 0
+for dirpath, dirs, files in os.walk("."):
+    path = dirpath.split('/')
+    print '|', (len(path)) * '---', '[', os.path.basename(dirpath), ']'
+    for f in files:
+        this_fullpath = dirpath + '/' + f
+        this_filesize = os.path.getsize(this_fullpath)
+        # print '|', len(path)*'---', f, ' ', this_filesize
+        cursor = conn.execute("SELECT ID, path FROM fileIndex WHERE filename=? AND size=?", (f, str(this_filesize)))
+        result_length = len(cursor.fetchall())
+        if(result_length == 0):
+            conn.execute("INSERT INTO fileIndex (path, filename, size) VALUES(?, ?, ?)", (this_fullpath, f, str(this_filesize)))
+            print "inserted: ", this_fullpath
+        else:
+            cursor = conn.execute("SELECT ID, path FROM fileIndex WHERE filename=? AND size=?", (f, str(this_filesize)))
+            found_file_path = False
+            for row in cursor:
+                if(row[1] == this_fullpath):
+                    found_file_path = True
+                else:
+                    print "Found Duplicate:", this_fullpath, " in:", row[1]
+                    duplicateSpaceUsed += this_filesize
+                    print "duplicate Total=", duplicateSpaceUsed
+            if(found_file_path is False):
+                print "Unique file:", this_fullpath
+                conn.execute("INSERT INTO fileIndex (path, filename, size) VALUES(?, ?, ?)", (this_fullpath, f, str(this_filesize)))
+                print "inserted: ", this_fullpath
 
-conn.commit();
+conn.commit()
 # cursor = conn.execute("SELECT id, path, filename, size  from fileIndex")
 # for row in cursor:
 #    print "ID = ", row[0]
